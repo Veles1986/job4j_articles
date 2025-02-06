790177684d72494fff20c82c615907e7f179cc52
Veles1986 <ilyos.rafa@mail.com>
2025-02-07 02:36:16 +0500

    Сортировка слиянием [#504997 #531677]

--- src/test/java/ru/job4j/algo/sort/MergeTest.java
+++ src/test/java/ru/job4j/algo/sort/MergeTest.java
@@ -6,13 +6,13 @@ import static org.assertj.core.api.Assertions.*;
 
 class MergeTest {
     @Test
-    void whenSortedThenOkFirst(){
+    void whenSortedThenOkFirst() {
         int[] array = {10, 4, 6, 4, 8, -13, 2, 3};
         assertThat(Merge.mergesort(array)).containsExactly(-13, 2, 3, 4, 4, 6, 8, 10);
     }
 
     @Test
-    void whenSortedThenOkSecond(){
+    void whenSortedThenOkSecond() {
         int[] array = {12, 14, 4, 7, 15, 0, -20};
         assertThat(Merge.mergesort(array)).containsExactly(-20, 0, 4, 7, 12, 14, 15);
     }
