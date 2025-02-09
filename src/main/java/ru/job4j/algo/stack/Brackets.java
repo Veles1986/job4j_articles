7f7c27d6cb91ae7a436b8084aaaefa228834a338
Veles1986 <ilyos.rafa@mail.com>
2025-02-09 14:50:28 +0500

    Очереди и стеки [#505157 #531941]

--- src/main/java/ru/job4j/algo/stack/Brackets.java
+++ src/main/java/ru/job4j/algo/stack/Brackets.java
@@ -3,16 +3,16 @@ package ru.job4j.algo.stack;
 import java.util.*;
 
 class Brackets {
-    private static final Map<Character, Character> brackets = new HashMap<>(Map.of('(', ')', '[', ']', '{', '}'));
+    private static final Map<Character, Character> BRACKETS = new HashMap<>(Map.of('(', ')', '[', ']', '{', '}'));
 
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
         char[] input = s.toCharArray();
         for (char symbol : input) {
-            if (brackets.containsKey(symbol)) {
+            if (BRACKETS.containsKey(symbol)) {
                 stack.push(symbol);
-            } else if (brackets.containsValue(symbol)) {
-                if (stack.empty() || !brackets.get(stack.pop()).equals(symbol)) {
+            } else if (BRACKETS.containsValue(symbol)) {
+                if (stack.empty() || !BRACKETS.get(stack.pop()).equals(symbol)) {
                     return false;
                 }
             } else {
